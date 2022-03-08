1c013227682075e1c101bf3247e550fc1c08ab0a
cgomez22@ucm.es <cgomez22@ucm.es>
2022-03-08 17:54:21 +0100

    08-03-22	Ahora si creación de clases

---
+++ Codigo/Prototipo_1/src/main/java/Logic/City/City.java
@@ -0,0 +1,10 @@
+package Logic.City;
+
+import java.io.Serializable;
+
+import jakarta.persistence.*;
+
+@Entity
+public class City implements Serializable{
+
+}
---
+++ Codigo/Prototipo_1/src/main/java/Presentation/City/VBajaCity.java
@@ -0,0 +1,5 @@
+package Presentation.City;
+
+public class VBajaCity {
+
+}
