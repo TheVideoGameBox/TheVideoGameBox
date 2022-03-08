9c25f68cff7b667a1bf2f018df36402ba41bc552
AGomezSittima <agomezsittimadeveloper@gmail.com>
2022-03-08 22:06:33 +0100

    08/03/22 - Baja Persona

--- Codigo/Prototipo_mongDB_1/src/main/java/Data/Person/DAOPersonImp.java
+++ Codigo/Prototipo_mongDB_1/src/main/java/Data/Person/DAOPersonImp.java
@@ -5,10 +5,14 @@ import Logic.Person.TPerson;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
+
+
+import static com.mongodb.client.model.Filters.eq;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
@@ -27,20 +31,38 @@ public class DAOPersonImp {
             CodecRegistry pojoCodeRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
             MongoDatabase db = mongoClient.getDatabase(DATABASE).withCodecRegistry(pojoCodeRegistry);
             Person person = new Person(tPerson);
+
             db.getCollection("personas", Person.class).insertOne(person);
+        } catch (Exception e) {
+            return "";
+        }
+
+        return "";
+    }
+
+    public String delete(String nif) {
+        try(MongoClient mongoClient = MongoClients.create(URL)) {
+            CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+            CodecRegistry pojoCodeRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+            MongoDatabase db = mongoClient.getDatabase(DATABASE).withCodecRegistry(pojoCodeRegistry);
+
+            MongoCollection<Person> collection = db.getCollection("personas", Person.class);
+
+            List<Person> personas = new ArrayList<Person>();
+            collection.find(eq("nif", nif)).into(personas);
+
+            if(personas.size() == 1){
+                Person persona = new Person(personas.get(0).toTransfer());
+                persona.setActivo(false);
+
+                collection.replaceOne(eq("nif", nif), persona);
+            }
 
         } catch (Exception e) {
             return "";
         }
+
         return "";
-    }
-
-    public int delete(String id) {
-        return -1;
-    }
-
-    public TPerson read(String id) {
-        return null;
     }
 
     public List<TPerson> readAll() {
--- Codigo/Prototipo_mongDB_1/src/main/java/Logic/Person/SAPersonImp.java
+++ Codigo/Prototipo_mongDB_1/src/main/java/Logic/Person/SAPersonImp.java
@@ -7,21 +7,33 @@ import java.util.List;
 public class SAPersonImp {
 
     public String add(TPerson tPerson) {
+        if (tPerson.getNif().length() != 9)
+            return "";
+        else if (!tPerson.getNif().substring(0, 8).matches("^[0-9]+$"))
+            return "";
+        else if (!Character.isAlphabetic(tPerson.getNif().charAt(8)))
+            return "";
+
         DAOPersonImp person = new DAOPersonImp();
-        if(tPerson.getNif().length() == 9) {
-            person.add(tPerson);
-        }
-        else return "";
+
+        person.add(tPerson);
 
         return "";
     }
 
-    public int delete(String id) {
-        return 0;
-    }
+    public String delete(String nif) {
+        if (nif.length() != 9)
+            return "";
+        else if (!nif.substring(0, 8).matches("^[0-9]+$"))
+            return "";
+        else if (!Character.isAlphabetic(nif.charAt(8)))
+            return "";
 
-    public TPerson read(String id) {
-        return null;
+        DAOPersonImp person = new DAOPersonImp();
+
+        person.delete(nif);
+
+        return "";
     }
 
     public List<TPerson> readAll() {
