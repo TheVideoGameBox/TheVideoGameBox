57b08da0ad201f155203a42865c0091ec9aa61f2
Javi <devicentevazquezjavier@gmail.com>
2022-03-18 17:03:42 +0100

    La funcion SearchAllByName del DAO creada

--- Codigo/TheVideoGameBox/src/main/java/Data/Game/DAOGame.java
+++ Codigo/TheVideoGameBox/src/main/java/Data/Game/DAOGame.java
@@ -1,4 +1,33 @@
 package Data.Game;
 
+import Data.Connection;
+import Logic.Game.Game;
+import Logic.Game.TGame;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class DAOGame {
+
+    public List<TGame> SearchAllByName(String name) {
+        List<TGame> result = new ArrayList<>();
+        try {
+            MongoDatabase db = Connection.getInstance().getConnection();
+            FindIterable<Game> iter = db.getCollection("games", Game.class).find();
+            for(Game g : iter) {
+                if(g.getName().toLowerCase().contains(name.toLowerCase())) {
+                    result.add(g.toTransfer());
+                }
+            }
+        } catch (MongoException e) {
+            return result;
+        }
+        return result;
+    }
+
+
 }
