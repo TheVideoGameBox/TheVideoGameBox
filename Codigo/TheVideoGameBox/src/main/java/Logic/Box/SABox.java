642569784a22e1d1b7f48802993fba7339866479
Javi <devicentevazquezjavier@gmail.com>
2022-03-18 17:25:28 +0100

    Creado SearchAllByName en el SA

--- Codigo/TheVideoGameBox/src/main/java/Logic/Box/SABox.java
+++ Codigo/TheVideoGameBox/src/main/java/Logic/Box/SABox.java
@@ -1,4 +1,11 @@
 package Logic.Box;
 
+import Logic.Game.TGame;
+
+import java.util.List;
+
 public interface SABox {
+
+    public List<TGame> SearchAllByname(String name);
+
 }
--- Codigo/TheVideoGameBox/src/main/java/Logic/Box/SABoxImp.java
+++ Codigo/TheVideoGameBox/src/main/java/Logic/Box/SABoxImp.java
@@ -1,4 +1,16 @@
 package Logic.Box;
 
+import Data.DAOAbstractFactory;
+import Logic.Game.TGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SABoxImp implements SABox {
+
+    @Override
+    public List<TGame> SearchAllByname(String name) {
+        List<TGame> result = new ArrayList<>();
+        return result = DAOAbstractFactory.getInstance().createDAOGame().SearchAllByName(name);
+    }
 }
