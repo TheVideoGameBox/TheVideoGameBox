ed9f6589636843be95e9faa4dbc5ae9c0ec685f9
Javi <devicentevazquezjavier@gmail.com>
2022-03-18 17:44:29 +0100

    Creado el comando SearchAllByName y las factorias de Command

--- Codigo/TheVideoGameBox/src/main/java/Presentation/Controller/ApplicationControllerImp.java
+++ Codigo/TheVideoGameBox/src/main/java/Presentation/Controller/ApplicationControllerImp.java
@@ -2,7 +2,7 @@ package Presentation.Controller;
 
 public class ApplicationControllerImp extends ApplicationController {
     @Override
-    public void action(int evento, Object datos) {
+    public void action(int event, Object data) {
         //1. Get command with CommandAbstractFactory
         //2. Execute command
         //3. Get view with ViewAbstractFactory and update it
