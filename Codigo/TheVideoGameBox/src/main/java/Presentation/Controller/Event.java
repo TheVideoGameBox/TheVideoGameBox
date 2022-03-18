a680f0f5e82a8487111302092938abf31412e738
Javi <devicentevazquezjavier@gmail.com>
2022-03-18 16:44:23 +0100

    Codigos de eventos para SearchAllByName creados

--- Codigo/TheVideoGameBox/src/main/java/Presentation/Controller/Event.java
+++ Codigo/TheVideoGameBox/src/main/java/Presentation/Controller/Event.java
@@ -1,4 +1,25 @@
 package Presentation.Controller;
 
 public class Event {
+
+    // Codigos de eventos para la UI (0-100)
+    public static final int VIEW = 1;
+
+
+
+    //  Codigos de eventos para GAME [100-300)
+    public static final int SearchAllByName = 101;
+    public static final int SearchAllByNameOk = 102;
+    public static final int SearchAllByNameKo = 103;
+
+
+
+    // Codigos de eventos para USER [300-500)
+
+
+
+
+
+    // Codigos de eventos para BOX [500-700)
+
 }
