62439bd4599853a6bc2b5b7e436fa4537a080bdc
AGomezSittima <agomezsittimadeveloper@gmail.com>
2022-03-20 00:07:30 +0100

    Codigo de Python para insertar los juegos en la base de datos

---
+++ Codigo/API/main.py
@@ -0,0 +1,11 @@
+from json import load
+from pymongo import MongoClient
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb+srv://MongoDB-GPS:ZUCUvvYbKW@gps.iox2a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    db = client['TVGB']
+    games = db.get_collection('games')
+
+    with open('games.json') as file:
+        file_data = load(file)
+        games.insert_one(file_data)
