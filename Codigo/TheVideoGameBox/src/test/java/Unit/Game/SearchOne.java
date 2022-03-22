ee9cf735ba3c0c255eca9e8f9ec1ec57fc1cc88a
Javi <devicentevazquezjavier@gmail.com>
2022-03-22 21:49:04 +0100

    JUnit SearchOne arreglado

--- Codigo/TheVideoGameBox/src/test/java/Unit/Game/SearchOne.java
+++ Codigo/TheVideoGameBox/src/test/java/Unit/Game/SearchOne.java
@@ -1,21 +1,30 @@
 package Unit.Game;
 
+import Data.Connection;
+import Logic.Game.Game;
 import Logic.Game.SAGame;
 import Logic.Game.TGame;
 import Logic.SAAbstractFactory;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import org.bson.types.ObjectId;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
 public class SearchOne {
 
     private static SAGame saGame;
-    private final ObjectId _id = new ObjectId("623a0636207a93d657bf5f8b");
+    private static ObjectId _id;
 
     @BeforeClass
-    public static void init(){
+    public static void init() {
+        MongoDatabase db = Connection.getInstance().getConnection();
+        MongoCollection<Game> games = db.getCollection("games", Game.class);
+        _id = Objects.requireNonNull(games.find().first()).getId();
         saGame = SAAbstractFactory.getInstance().createSAGame();
     }
 
