45fe05cd493d485618152261d7a7fd512648bd09
daavidelias <deividelias2000@gmail.com>
2022-03-24 00:23:55 +0100

    Vista y Comando CrearBox (falta comentar y organizar código de la vista)

--- Codigo/TheVideoGameBox/src/main/java/Presentation/View/Box/ViewCreateBox.java
+++ Codigo/TheVideoGameBox/src/main/java/Presentation/View/Box/ViewCreateBox.java
@@ -1,166 +1,329 @@
 package Presentation.View.Box;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-import Logic.Box.SABoxImp;
+import Logic.Box.Category;
+import Logic.Box.Privacy;
 import Logic.Box.TBox;
+import Presentation.Controller.ApplicationController;
 import Presentation.Controller.Context;
+import Presentation.Controller.Event;
 import Presentation.View.IView;
 import Presentation.View.Main.JPanelConFondo;
 import Presentation.View.Main.ViewPrincipal;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.AbstractListModel;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JComboBox;
+import javax.swing.UIManager;
+import java.awt.SystemColor;
 
 public class ViewCreateBox extends JFrame implements IView{
-	//Falta que hacer con la privacidad; donde controlarla.
-	private ArrayList<JTextField> textFields;
-	private boolean privacy;
-	
-	public ViewCreateBox() {
-		super();
-		textFields = new ArrayList<JTextField>();
-		initGUI();
-	}
-	
-	public void initGUI() {
-		this.getContentPane().removeAll();
-		this.setPreferredSize(new Dimension(900, 550));
-		this.setLocation(400, 100);
 
-		JPanelConFondo mainpanel = new JPanelConFondo();
-		mainpanel.setLayout(new BoxLayout(mainpanel,BoxLayout.Y_AXIS));
-		mainpanel.setImagen(new ImageIcon(getClass().getClassLoader().getResource("fondo_triangular.png")).getImage());
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.add(mainpanel);
+	private JPanelConFondo mainPanel = new JPanelConFondo();
+	private JPanel backButtonContainer;
+	private JTextField textFieldName;
+	private JTextField textFieldDescription;
+	private JCheckBox checkBoxShooter;
+	private JCheckBox checkBoxStrategy;
+	private JCheckBox checkBoxIndie;
+	private JCheckBox checkBoxRPG;
+	private JCheckBox checkBoxRacing;
+	private JCheckBox checkBoxSandbox;
+	private JCheckBox checkBoxHorror;
+	private JCheckBox checkBoxSports;
+	private JCheckBox checkBoxSurvival;
+	private JPanel panelCategories;
+	private JPanel midPanel;
+	private JComboBox comboBoxPrivacy;
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewCreateBox() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().removeAll();
+		setPreferredSize(new Dimension(900, 550));
+		setLocation(400, 100);
+		setBounds(100, 100, 887, 536);
+		mainPanel = new JPanelConFondo();
+		mainPanel.setBorder(null);
+		mainPanel.setImagen(new ImageIcon(getClass().getClassLoader().getResource("fondo_triangular.png")).getImage());
+		getContentPane().add(mainPanel);
+		mainPanel.setLayout(null);
 		
-		JPanel backButtonContainer = backButtonContainer();
-		mainpanel.add(Box.createRigidArea(new Dimension(0,20)));
-		mainpanel.add(backButtonContainer);
-		ArrayList<String> names = new ArrayList<String>();
-		names.add("Nombre");
-		names.add("Description");
-		names.add("Category");
-		
-		//creando la forma
-		JPanel midPanel = new JPanel();
+		midPanel = new JPanel();
+		midPanel.setBounds(203, 23, 556, 446);
 		midPanel.setOpaque(false);
-		JPanel formContainer = new JPanel();
-		formContainer.setLayout(new BoxLayout(formContainer, BoxLayout.Y_AXIS));
-		formContainer.setPreferredSize(new Dimension(400, 500));
-		formContainer.setAlignmentX(CENTER_ALIGNMENT);
-		formContainer.setAlignmentY(CENTER_ALIGNMENT);
-		formContainer.setOpaque(false);
-		formContainer.setBorder(new EmptyBorder(20, 20, 20, 20));
-		formContainer.setAutoscrolls(true);
+		mainPanel.add(midPanel);
+		midPanel.setLayout(null);
 		
-		//TITULO
+	
+		
 		JLabel titleLabel = new JLabel("Create Box");
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setAlignmentX(CENTER_ALIGNMENT);
 		titleLabel.setFont(new Font("Leelawadee", Font.BOLD, 40));
-		titleLabel.setForeground(new Color(64, 147, 255));
+		titleLabel.setBounds(158, 22, 225, 59);
+		titleLabel.setForeground(SystemColor.controlHighlight);
+		midPanel.add(titleLabel);
 		
-		//BOTON SUBMIT
-		JButton submitButton = new JButton("Submit");
-		submitButton.setMaximumSize(new Dimension(500, 30));
-		submitButton.setAlignmentX(CENTER_ALIGNMENT);
-		submitButton.setBackground(new Color(64, 147, 255));
-		submitButton.setForeground(Color.white);
-		submitButton.setFont(new Font("Leelawadee", Font.BOLD, 15));
-		submitButton.setFocusPainted(false);
-		submitButton.setBorderPainted(false);
-		submitButton.addActionListener(new ActionListener() {
+		textFieldName = new JTextField();
+		textFieldName.setToolTipText("Type new Box name");
+		textFieldName.setFont(new Font("Leelawadee", Font.PLAIN, 13));
+		textFieldName.setBounds(81, 114, 401, 30);
+		midPanel.add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		JLabel labelName = new JLabel("Name:");
+		labelName.setToolTipText("");
+		labelName.setForeground(Color.WHITE);
+		labelName.setFont(new Font("Leelawadee", Font.BOLD, 15));
+		labelName.setAlignmentX(CENTER_ALIGNMENT);
+		labelName.setBounds(257, 97, 66, 14);
+		midPanel.add(labelName);
+		
+		JLabel labelDescription = new JLabel("Description:");
+		labelDescription.setToolTipText("");
+		labelDescription.setForeground(Color.WHITE);
+		labelDescription.setFont(new Font("Leelawadee", Font.BOLD, 15));
+		labelDescription.setAlignmentX(0.5f);
+		labelDescription.setBounds(238, 162, 97, 14);
+		midPanel.add(labelDescription);
+		
+		textFieldDescription = new JTextField();
+		textFieldDescription.setToolTipText("Type new Box description");
+		textFieldDescription.setFont(new Font("Leelawadee", Font.PLAIN, 13));
+		textFieldDescription.setColumns(10);
+		textFieldDescription.setBounds(81, 180, 401, 30);
+		midPanel.add(textFieldDescription);
+		
+		JLabel labelCategories = new JLabel("Categories:");
+		labelCategories.setToolTipText("Select categories");
+		labelCategories.setForeground(Color.WHITE);
+		labelCategories.setFont(new Font("Leelawadee", Font.BOLD, 15));
+		labelCategories.setAlignmentX(0.5f);
+		labelCategories.setBounds(36, 277, 88, 19);
+		midPanel.add(labelCategories);
+		
+		
+		
+		comboBoxPrivacy = new JComboBox(Privacy.values());
+		comboBoxPrivacy.setBackground(UIManager.getColor("Button.light"));
+		comboBoxPrivacy.setFont(new Font("Leelawadee", Font.PLAIN, 14));
+		comboBoxPrivacy.setBounds(395, 276, 87, 22);
+		midPanel.add(comboBoxPrivacy);
+		
+		JButton btnCreate = new JButton("CREATE");
+		btnCreate.setForeground(Color.WHITE);
+		btnCreate.setFont(new Font("Leelawadee", Font.BOLD, 15));
+		btnCreate.setBounds(232, 378, 127, 39);
+		btnCreate.setBackground(new Color(64, 147, 255));
+		btnCreate.setFocusable(false);
+		btnCreate.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(textFields.get(0).getText().length() > 2 && textFields.get(1).getText().length() > 2 && textFields.get(2).getText().length() > 2) {
-					//TBox b = new TBox(textFields.get(0).getText(), textFields.get(1).getText(), privacy ,textFields.get(2).getText());
-
-					SABoxImp saBoxImp = new SABoxImp();
-					//saBoxImp.createBox(b);
-					
-					ViewPrincipal principal = new ViewPrincipal();
-					dispose();
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "Rellene todos los campos, por favor");
-				}
+				createBox();
 			}
 		});
+		midPanel.add(btnCreate);
 		
-		// CONSTRUIR COMPONENTE
-		formContainer.add(titleLabel);
-		formContainer.add(Box.createRigidArea(new Dimension(10, 20)));
-		for (String name : names) {
-			formContainer.add(createFormField(name));
-			formContainer.add(Box.createRigidArea(new Dimension(10, 20)));
-		}
-		formContainer.add(submitButton);
+		addPanelCheckboxes();
 		
-		midPanel.add(formContainer);
 		
-		mainpanel.add(Box.createRigidArea(new Dimension(0,80)));
-		mainpanel.add(midPanel);
+		backButtonContainer = new JPanel();
+		backButtonContainer.setLayout(null);
+		backButtonContainer.setOpaque(false);
+		backButtonContainer.setBounds(10, 11, 196, 67);
+		addBackButton();
+		mainPanel.add(backButtonContainer);
 		
-		this.pack();
+		
+		
+		
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
-	
-	private JPanel createFormField(String title) {
-		JPanel container = new JPanel();
-		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-		container.setOpaque(false);
-		container.setAlignmentX(CENTER_ALIGNMENT);
 
-		JLabel label = new JLabel(title);
-		label.setFont(new Font("Leelawadee", Font.BOLD, 15));
-		label.setAlignmentX(CENTER_ALIGNMENT);
-
-		JTextField input = new JTextField(6);
-		input.setMaximumSize(new Dimension(400, 30));
-		input.setPreferredSize(new Dimension(400, 30));
-		textFields.add(input);
-
-		container.add(label);
-		container.add(input);
-
-		return container;
+	protected void createBox() {
+		//Capturar datos
+		String name = textFieldName.getText();
+		String description = textFieldDescription.getText();
+		List<Category> categories = getCategories();
+		Privacy privacy = getPrivacy();
+		
+		TBox box = new TBox(name, description, privacy, categories);
+		
+		ApplicationController.getInstance().action(new Context(Event.CREATE_BOX, box));
+		
+		ViewPrincipal principal = new ViewPrincipal();
+		dispose();
+		
 	}
 
-	
-	private JPanel backButtonContainer(){
-		//BACK BUTTON
-		JPanel backButtonContainer = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		backButtonContainer.setMaximumSize(new Dimension(1000, 50));
-		backButtonContainer.setOpaque(false);
+	private Privacy getPrivacy() {
+		String privacy=comboBoxPrivacy.getSelectedItem().toString();
 		
-		JButton backButton = new JButton("Volver al Principio");
+		if(privacy.equals(Privacy.PRIVATE.toString()))
+			return Privacy.PRIVATE;
+		else
+			return Privacy.PUBLIC;
+	}
+
+	private List<Category> getCategories() {
+		List<Category> categories = new ArrayList<>();
+		
+		if(checkBoxShooter.isSelected()) {
+			categories.add(Category.SHOOTER);
+		}
+		if(checkBoxStrategy.isSelected()) {
+			categories.add(Category.STRATEGY);
+		}
+		if(checkBoxIndie.isSelected()) {
+			categories.add(Category.INDIE);
+		}
+		if(checkBoxRPG.isSelected()) {
+			categories.add(Category.RPG);
+		}
+		if(checkBoxRacing.isSelected()) {
+			categories.add(Category.RACING);
+		}
+		if(checkBoxSandbox.isSelected()) {
+			categories.add(Category.SANDBOX);
+		}
+		if(checkBoxHorror.isSelected()) {
+			categories.add(Category.HORROR);
+		}
+		if(checkBoxSports.isSelected()) {
+			categories.add(Category.SPORTS);
+		}
+		if(checkBoxSurvival.isSelected()) {
+			categories.add(Category.SURVIVAL);
+		}
+		return categories;
+	}
+
+	private void addPanelCheckboxes() {
+		panelCategories = new JPanel();
+		panelCategories.setBounds(120, 221, 215, 143);
+		panelCategories.setOpaque(false);
+		panelCategories.setLayout(null);
+		midPanel.add(panelCategories);
+		
+		checkBoxShooter = new JCheckBox("Shooter");
+		checkBoxShooter.setOpaque(false);
+		checkBoxShooter.setForeground(Color.WHITE);
+		checkBoxShooter.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxShooter.setFocusable(false);
+		checkBoxShooter.setBounds(6, 7, 83, 23);
+		panelCategories.add(checkBoxShooter);
+		
+		checkBoxStrategy = new JCheckBox("Estrategia");
+		checkBoxStrategy.setOpaque(false);
+		checkBoxStrategy.setForeground(Color.WHITE);
+		checkBoxStrategy.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxStrategy.setFocusable(false);
+		checkBoxStrategy.setBounds(6, 33, 91, 23);
+		panelCategories.add(checkBoxStrategy);
+		
+		checkBoxIndie = new JCheckBox("Indie");
+		checkBoxIndie.setOpaque(false);
+		checkBoxIndie.setForeground(Color.WHITE);
+		checkBoxIndie.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxIndie.setFocusable(false);
+		checkBoxIndie.setBounds(6, 59, 83, 23);
+		panelCategories.add(checkBoxIndie);
+		
+		checkBoxRPG = new JCheckBox("RPG");
+		checkBoxRPG.setOpaque(false);
+		checkBoxRPG.setForeground(Color.WHITE);
+		checkBoxRPG.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxRPG.setFocusable(false);
+		checkBoxRPG.setBounds(6, 85, 83, 23);
+		panelCategories.add(checkBoxRPG);
+		
+		checkBoxRacing = new JCheckBox("Racing");
+		checkBoxRacing.setOpaque(false);
+		checkBoxRacing.setForeground(Color.WHITE);
+		checkBoxRacing.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxRacing.setFocusable(false);
+		checkBoxRacing.setBounds(6, 111, 83, 23);
+		panelCategories.add(checkBoxRacing);
+		
+		checkBoxSandbox = new JCheckBox("Sandbox");
+		checkBoxSandbox.setHorizontalAlignment(SwingConstants.CENTER);
+		checkBoxSandbox.setOpaque(false);
+		checkBoxSandbox.setForeground(Color.WHITE);
+		checkBoxSandbox.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxSandbox.setFocusable(false);
+		checkBoxSandbox.setBounds(99, 20, 83, 23);
+		panelCategories.add(checkBoxSandbox);
+		
+		checkBoxHorror = new JCheckBox("Terror");
+		checkBoxHorror.setOpaque(false);
+		checkBoxHorror.setForeground(Color.WHITE);
+		checkBoxHorror.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxHorror.setFocusable(false);
+		checkBoxHorror.setBounds(101, 46, 81, 23);
+		panelCategories.add(checkBoxHorror);
+		
+		checkBoxSports = new JCheckBox("Deporte");
+		checkBoxSports.setOpaque(false);
+		checkBoxSports.setForeground(Color.WHITE);
+		checkBoxSports.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxSports.setFocusable(false);
+		checkBoxSports.setBounds(101, 72, 81, 23);
+		panelCategories.add(checkBoxSports);
+		
+		checkBoxSurvival = new JCheckBox("Supervivencia");
+		checkBoxSurvival.setOpaque(false);
+		checkBoxSurvival.setForeground(Color.WHITE);
+		checkBoxSurvival.setFont(new Font("Leelawadee", Font.BOLD, 13));
+		checkBoxSurvival.setFocusable(false);
+		checkBoxSurvival.setBounds(101, 96, 114, 23);
+		panelCategories.add(checkBoxSurvival);
+		
+		JLabel labelPrivacy = new JLabel("Privacy:");
+		labelPrivacy.setToolTipText("Select privacy");
+		labelPrivacy.setForeground(Color.WHITE);
+		labelPrivacy.setFont(new Font("Leelawadee", Font.BOLD, 15));
+		labelPrivacy.setAlignmentX(0.5f);
+		labelPrivacy.setBounds(334, 277, 66, 19);
+		midPanel.add(labelPrivacy);
+	}
+
+	private void addBackButton() {
+		JButton backButton = new JButton("Go back");
+		backButton.setForeground(Color.WHITE);
 		backButton.setFont(new Font("Leelawadee", Font.BOLD, 15));
 		backButton.setBackground(new Color(64, 147, 255));
-		backButton.setForeground(Color.white);
-		backButton.setToolTipText("Volver a la pagina principal");
-		backButton.setPreferredSize(new Dimension(180, 50));
-		backButton.setFocusPainted(false);
-		backButton.setAlignmentX(LEFT_ALIGNMENT);
-		backButton.setBorder(null);
-		//backButton.setBorder(BorderFactory.createBevelBorder(0));
+		backButton.setBounds(36, 11, 119, 45);
+		backButton.setFocusable(false);
 		backButtonContainer.add(backButton);
-	
+		
 		backButton.addActionListener(new ActionListener(){
 
 			@Override
@@ -171,13 +334,20 @@ public class ViewCreateBox extends JFrame implements IView{
 			
 		});
 		
-		return backButtonContainer;
 	}
 
 	@Override
 	public void update(Context context) {
-		// TODO Auto-generated method stub
 		
+		switch(context.getEvent()) {
+		case Event.RES_CREATE_BOX_OK:
+			JOptionPane.showMessageDialog(null, "Box creada con id: " + (String)context.getData());
+			break;
+		case Event.RES_CREATE_BOX_KO:
+			JOptionPane.showMessageDialog(null, "No se pudo crear la Box");
+		}
+		
+		ApplicationController.getInstance().action(new Context(Event.VIEW, null));
+		dispose();
 	}
-
 }
