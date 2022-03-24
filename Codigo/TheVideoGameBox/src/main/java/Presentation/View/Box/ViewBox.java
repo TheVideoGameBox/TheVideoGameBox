58aeb6e73a8b2d8f73107a9ab842af1fc7984fc9
LordSama <juanromo@ucm.es>
2022-03-24 17:01:11 +0100

    View Box terminada

--- Codigo/TheVideoGameBox/src/main/java/Presentation/View/Box/ViewBox.java
+++ Codigo/TheVideoGameBox/src/main/java/Presentation/View/Box/ViewBox.java
@@ -20,15 +20,19 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import Logic.Box.TBox;
+import Presentation.Controller.ApplicationController;
 import Presentation.Controller.Context;
+import Presentation.Controller.Event;
 import Presentation.View.IView;
 import Presentation.View.Main.BorderTitle;
 import Presentation.View.Main.JPanelConFondo;
+import Presentation.View.Main.JPanelRound;
 
-public class ViewBox extends JFrame implements IView{
+public class ViewBox extends JFrame implements IView {
 
 	private static final long serialVersionUID = 1L;
 	TBox box;
@@ -94,25 +98,47 @@ public class ViewBox extends JFrame implements IView{
 		midPanel.setBorder(new BorderTitle("Info about box", Color.black));
 		
 		// Nombre de box
-		JLabel boxNameLabel = new JLabel(box.getName());
-		boxNameLabel.setAlignmentX(LEFT_ALIGNMENT);
-		boxNameLabel.setFont(new Font("Leelawadee", Font.BOLD, 30));
+		JLabel boxNameLabel = new JLabel("nombre box");		// Aqui es box.getName();
+		boxNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		//boxNameLabel.setAlignmentX(CENTER_ALIGNMENT);
+		boxNameLabel.setFont(new Font("Leelawadee", Font.BOLD, 20));
 		boxNameLabel.setForeground(Color.WHITE);
 		
+		midPanel.add(boxNameLabel);
+		midPanel.add(Box.createRigidArea(new Dimension(0, 15)));
+		midPanel.add(createAddGamePanel());
+		
+		return midPanel;
+	}
+	
+	private JPanel createAddGamePanel() {
+		JPanel addPanel = new JPanelRound();
+		addPanel.setMaximumSize(new Dimension(1150, 70));
+		addPanel.setMinimumSize(new Dimension(1150, 70));
+		addPanel.setLayout(new BoxLayout(addPanel, BoxLayout.Y_AXIS));
 		// En esta HU (Añadir un juego a una box), solamente va a haber un botón para añadir juegos. El resto de la vista
 		// de la box se realizará en Ver juegos de la box.
-		
+				
 		//ADD GAME 
-			// ADD GAME LABEL
+		// ADD GAME LABEL
 		JLabel addGameLabel = new JLabel("Search here to add a new game to the box");
-		addGameLabel.setAlignmentX(LEFT_ALIGNMENT);
-		addGameLabel.setFont(new Font("Leelawadee", Font.ITALIC, 30));
+		addGameLabel.setFont(new Font("ComicSans", Font.ITALIC, 15));
 		addGameLabel.setForeground(Color.WHITE);
+		//addPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		addPanel.add(addGameLabel);
+			
 		
-			// Input para introducir el nombre del juego que se desea añadir
+		
+		// Input para introducir el nombre del juego que se desea añadir
+		JPanel textButtonPanel = new JPanel();
+		//textButtonPanel.setMinimumSize(new Dimension(750, 150));
+		textButtonPanel.setLayout(new BoxLayout(textButtonPanel, BoxLayout.X_AXIS));
+		textButtonPanel.setOpaque(false);
+		
 		JTextField nameGame = new JTextField();
 		nameGame.setText("name of game");
 		nameGame.setForeground(Color.LIGHT_GRAY);
+		nameGame.setMaximumSize(new Dimension(600, 40));
 		nameGame.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -130,33 +156,38 @@ public class ViewBox extends JFrame implements IView{
 			public void mouseEntered(MouseEvent e) {}
 
 			@Override
-			public void mouseExited(MouseEvent e) {}
-			
+			public void mouseExited(MouseEvent e) {}			
 		});
 		
-			// ADD GAME BUTTON
-		JButton addGameButton = new JButton("Add a new game");
-		addGameButton.setIcon(new ImageIcon((getClass().getClassLoader().getResource("add_button.png"))));
-		addGameButton.setBackground(new Color(255, 255, 255));
-		addGameButton.setForeground(Color.white);
-		addGameButton.setBorder(BorderFactory.createBevelBorder(0));
+		addPanel.add(textButtonPanel);
+		textButtonPanel.add(Box.createRigidArea(new Dimension(15, 10)));
+		textButtonPanel.add(nameGame);
+				
+		// ADD GAME BUTTON
+		JButton addGameButton = new JButton();
+		addGameButton.setPreferredSize(new Dimension(40,40));
+		addGameButton.setMaximumSize(new Dimension(40, 40));
+		addGameButton.setMinimumSize(new Dimension(40, 40));
+		addGameButton.setIcon(new ImageIcon((getClass().getClassLoader().getResource("search_icon.png"))));
 		addGameButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
 					// Llamamos al evento de search all by name, pues la vista va a ser la misma, pero cambiará que al hacer click
 					// en un juego, en lugar de mostrar su info, se añadirá a la box.
-					//Controller
+					String name = nameGame.getText();
+					ApplicationController.getInstance().action(new Context(Event.SEARCH_ALL_BY_NAME, name));;
 				} catch(Exception e1) {
 					JOptionPane.showMessageDialog(null, "Incorrect name format");
 				}
 			}
-			
 		});
+		textButtonPanel.add(Box.createRigidArea(new Dimension(5,0)));
+		textButtonPanel.add(addGameButton);
 		
-		return midPanel;
+		return addPanel;
 	}
-
+	
 	@Override
 	public void update(Context context) {
 		// TODO Auto-generated method stub
