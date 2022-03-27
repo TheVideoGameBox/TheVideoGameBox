f3cefa0196d69bd6e791255c391c7d8b936ffa11
carlo <carlo@DESKTOP-JOUCQG7>
2022-03-27 19:33:47 +0200

    27/03/2022 Creación transfer de user

--- Codigo/TheVideoGameBox/src/main/java/Logic/User/TUser.java
+++ Codigo/TheVideoGameBox/src/main/java/Logic/User/TUser.java
@@ -1,4 +1,70 @@
 package Logic.User;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
 public class TUser {
-}
+
+	    private ObjectId id;
+	    private String email;
+	    private String username;
+	    private String password;
+	
+
+	    public TUser(String email, String username, String password) {
+	        this.email = email;
+	        this.username = username;
+	        this.password = password;
+	    }
+
+	    public TUser(ObjectId id, String email, String username, String password) {
+	        this.id = id;
+	        this.email = email;
+	        this.username = username;
+	        this.password = password;
+	    }
+
+	    public ObjectId getId() {
+	        return id;
+	    }
+
+	    public void setId(ObjectId id) {
+	        this.id = id;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "{" +
+	                "id=" + id +
+	                ", username='" + username + '\'' +
+	                ", email='" + email + '\'' +
+	                ", password=" + password +
+	                '}';
+	    }
+	}
+
+
---
+++ Codigo/TheVideoGameBox/src/main/java/Logic/User/User.java
@@ -0,0 +1,97 @@
+package Logic.User;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+import Logic.Game.TGame;
+
+public class User {
+	
+	    private ObjectId id;
+	    private String email;
+	    private String username;
+	    private String password;
+	
+
+	    public User() {
+	    	
+	    }
+
+	    public User(String email, String username, String password) {
+	        this.email = email;
+	        this.username = username;
+	        this.password = password;
+	    }
+
+	    public User(ObjectId id, String email, String username, String password) {
+	        this.id = id;
+	        this.email = email;
+	        this.username = username;
+	        this.password = password;
+	    }
+	    public User(TUser tUser){
+	        this.id = tUser.getId();
+	        this.email = tUser.getEmail();
+	        this.username = tUser.getUsername();
+	        this.password = tUser.getPassword();
+	    }
+
+	    //Getters and setters
+
+	    public ObjectId getId() {
+	        return id;
+	    }
+
+	    public void setId(ObjectId id) {
+	        this.id = id;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	    
+
+	    //Obtener todos los datos de un transfer
+	    public void setData(TUser tUser){
+	    	 this.id = tUser.getId();
+		        this.email = tUser.getEmail();
+		        this.username = tUser.getUsername();
+		        this.password = tUser.getPassword();
+	    }
+
+
+	    //Transformar una entidad de MongoDB a un transfer
+	    public TUser toTransfer(){
+	        return new TUser(id,email, username, password);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "{" +
+	                "id=" + id +
+	                ", username='" + username + '\'' +
+	                ", email='" + email + '\'' +
+	                ", password=" + password +
+	                '}';
+	    }
+}
