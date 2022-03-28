01701ba446a24b45500671eac637725ed40c0ef3
LordSama <juanromo@ucm.es>
2022-03-29 00:47:48 +0200

    view register

--- Codigo/TheVideoGameBox/src/main/java/Presentation/View/User/ViewRegister.java
+++ Codigo/TheVideoGameBox/src/main/java/Presentation/View/User/ViewRegister.java
@@ -1,5 +1,6 @@
 package Presentation.View.User;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -15,11 +16,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 import Presentation.Controller.ApplicationController;
 import Presentation.Controller.Context;
 import Presentation.Controller.Event;
 import Presentation.View.IView;
+import Presentation.View.Main.BorderTitle;
 import Presentation.View.Main.JPanelConFondo;
 
 public class ViewRegister extends JFrame implements IView {
@@ -35,89 +39,50 @@ public class ViewRegister extends JFrame implements IView {
 	private void init_GUI() {
 		this.setPreferredSize(new Dimension(1150, 750));
 		this.setLocation(400, 100);
-		
 		Image iconFrame = new ImageIcon(getClass().getClassLoader().getResource("caja_definitiva.png")).getImage();
 		this.setIconImage(iconFrame);
 		
+		JPanelConFondo mainpanel = new JPanelConFondo();
+		mainpanel.setLayout(new BorderLayout());
+		mainpanel.setImagen(new ImageIcon(getClass().getClassLoader().getResource("fondo_triangular.png")).getImage());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanelConFondo mainPanel = new JPanelConFondo();
-		mainPanel.setImagen(new ImageIcon(getClass().getClassLoader().getResource("fondo_triangular.png")).getImage());
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		mainPanel.setPreferredSize(new Dimension(500, 500));
-		this.add(mainPanel);
+		this.add(mainpanel);
 		
-		// CONTENT CONTAINER
-
-		JPanel contentContainer = new JPanel();
-		contentContainer.setLayout(new BoxLayout(contentContainer, BoxLayout.Y_AXIS));
-		contentContainer.setAlignmentX(CENTER_ALIGNMENT);
-		contentContainer.setOpaque(false);
-
-		JScrollPane scrollFrame = new JScrollPane(contentContainer);
-		contentContainer.setAutoscrolls(true);
-		scrollFrame.setOpaque(false);
-		scrollFrame.getViewport().setOpaque(false);
-
-		mainPanel.add(scrollFrame);
-				
-		// HEADER
-		JPanel headerContainer = new JPanel();
-		headerContainer.setMaximumSize(new Dimension(1200, 100));
-		headerContainer.setLayout(new BoxLayout(headerContainer, BoxLayout.X_AXIS));
-		headerContainer.setOpaque(false);
-		headerContainer.add(Box.createRigidArea(new Dimension(60, 0)));
+		JPanel topPanel = createTopPanel();
+		JPanel midPanel = createMidPanel();
 		
-		// TITLE
-		JLabel title = new JLabel("REGISTER");
-		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 25));
-				title.setAlignmentX(CENTER_ALIGNMENT);
-				title.setForeground(Color.white);
-				title.setFont(new Font("sans-serif", 1, 20));
-				headerContainer.add(title);
-				headerContainer.add(Box.createRigidArea(new Dimension(190, 0)));
-
-	
-		// ICONO DE MENU
-		JButton icon = new JButton();
-		icon.setIcon(new ImageIcon((getClass().getClassLoader().getResource("logo_small_blanco.png"))));
-		icon.setToolTipText("Back to main window");
-		icon.setBorderPainted(false);
-		icon.setOpaque(false);
-		icon.setContentAreaFilled(false);
-		icon.setFocusPainted(false);
-		icon.setAlignmentX(CENTER_ALIGNMENT);
-		headerContainer.add(icon);
-		headerContainer.add(Box.createRigidArea(new Dimension(270, 0)));
-
-		icon.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				ApplicationController.getInstance().action(new Context(Event.VIEW, null));
-				dispose();
-			}
-		});
+		mainpanel.add(topPanel, BorderLayout.NORTH);
+		mainpanel.add(midPanel, BorderLayout.SOUTH);
 		
-		// CONSTRUIR VISTA
-		contentContainer.add(headerContainer);
-		contentContainer.add(Box.createRigidArea(new Dimension(0, 10)));
-		
-		// HELP
-		JPanel helpPanel = new JPanel();
-		JLabel help = new JLabel("Push the TVGB logo to go back the main window");
-		help.setForeground(Color.white);
-		help.setAlignmentX(CENTER_ALIGNMENT);
-		help.setFont(new Font("Leelawadee", Font.BOLD, 13));
-		helpPanel.setOpaque(false);
-		helpPanel.setMaximumSize(new Dimension(1000, 40));
-		helpPanel.add(help);
-						
-		contentContainer.add(Box.createRigidArea(new Dimension(0, 10)));
-		contentContainer.add(helpPanel);
-		contentContainer.add(Box.createRigidArea(new Dimension(0, 10)));
-	
 		this.pack();
+		this.setResizable(true);
+		setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
+	
+private JPanel createTopPanel() {
+		
+		JPanel topPanel = new JPanel();
+		topPanel.setPreferredSize(new Dimension(1150, 100));
+		topPanel.setMaximumSize(new Dimension(1150, 100));
+		topPanel.setMinimumSize(new Dimension(1150, 100));
+		topPanel.setOpaque(false);
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+		
+		//ICONO DE LA APLICACION
+		JLabel icon = new JLabel();
+		icon.setIcon(new ImageIcon(getClass().getClassLoader().getResource("logo_small_blanco.png")));		
+		topPanel.add(Box.createRigidArea(new Dimension(110,0)));
+		topPanel.add(icon);
+		
+		return topPanel;
+    }
+
+private JPanel createMidPanel() {
+	JPanel midPanel = new JPanel();
+
+	return midPanel;
+}
 
 	@Override
 	public void update(Context context) {
