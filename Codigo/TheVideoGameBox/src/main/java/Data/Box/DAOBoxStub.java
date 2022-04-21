d895e609dddf37cf6e9461e6013e86bd1fde2f1d
AGomezSittima <agomezsittimadeveloper@gmail.com>
2022-04-21 14:18:16 +0200

    Se ha borrado el main.yml por alguna razon

--- Codigo/TheVideoGameBox/src/main/java/Data/Box/DAOBoxStub.java
+++ Codigo/TheVideoGameBox/src/main/java/Data/Box/DAOBoxStub.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class DAOBoxStub implements DAOBox {
-//La integracion continua funciona
     @Override
     public ObjectId create(TBox box) {
         return ObjectId.get();
